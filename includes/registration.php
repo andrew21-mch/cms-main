6e481b8b420e3bd1d0121f4f1d0b1d092bd611d7
andrew21-mch <nfonandrew73@gmail.com>
2021-05-20 18:07:41 +0100

    changed the linked of action page and added names

---
+++ includes/registration.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "../config.php";
+require_once "validations.inc.php";
+if(isset($_POST['register'])){
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password_repeat = $_POST['password_repeat'];
+    $user_email = $_POST["email"];
+
+    if(ValidatePass($password, $password_repeat)==true){
+    $sql = "INSERT INTO `wtaxy_user`(`user_name`, `user_email`, `user_pass`, `user_first_name`, `user_last_name`) VALUES ('$username','$user_email','$password','$first_name','$last_name')";
+    if(mysqli_query($conn,$sql)){
+        echo"<script>alert('Account Succesfully Created');</script>";
+    }
+    else
+    {
+    echo "ERROR:". $sql. "<br>". mysqli_error($conn);
+    }
+}
+else{
+    header("Location: ../register.html");
+}
+}
+    
